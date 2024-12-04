b6a1c2cc19294ec693f671779ba30dcdde66f3c3
ramana-07-web <ramana4562d07@gmail.com>
2024-12-04 15:23:22 +0530

    Update App.java

--- src/main/java/com/example/my_maven_project/App.java
+++ src/main/java/com/example/my_maven_project/App.java
@@ -6,5 +6,6 @@ package com.example.my_maven_project;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello!");
     }
 }
